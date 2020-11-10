496565603176983f6e53ade91aeb64fc553ca6be
Gizem Fitoz <fitozgizem@gmail.com>
2020-11-10 23:12:53 +0300

    MBIOS-9.2 do not show version info on production

--- MobvenIOSTI/AppDelegate.swift
+++ MobvenIOSTI/AppDelegate.swift
@@ -14,8 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        UIViewController.classInitForVersion
+        let showVersion = Bundle.main.infoDictionary?["ShowVersion"] as? String ?? "NO"
+        if showVersion == "YES" {
+            UIViewController.classInitForVersion
+        }
+        
         return true
     }
     
