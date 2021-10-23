625007d0ad0e291147c542cb4869027a29e5e68d
Harun DERE <harun.dere@basarsoft.com.tr>
2021-10-23 14:39:37 +0300

    MBIOS-3

--- MobvenIOSTI/API/Gist.swift
+++ MobvenIOSTI/API/Gist.swift
@@ -7,13 +7,33 @@
 //
 
 import Foundation
+/*
+{
+    "id": "8f28fda67396a49a6c46b0964be4f5a8",
+    "description": "Knowledge base",
+    "comments": 0,
+    "created_at": "2021-10-23T11:30:25Z",
+    "url": "https://api.github.com/gists/8f28fda67396a49a6c46b0964be4f5a8",
+    "node_id": "G_kwDOAS690toAIDhmMjhmZGE2NzM5NmE0OWE2YzQ2YjA5NjRiZTRmNWE4",
+    "git_push_url": "https://gist.github.com/8f28fda67396a49a6c46b0964be4f5a8.git",
+    "html_url": "https://gist.github.com/8f28fda67396a49a6c46b0964be4f5a8",
+    "git_pull_url": "https://gist.github.com/8f28fda67396a49a6c46b0964be4f5a8.git",
+    "updated_at": "2021-10-23T11:30:25Z",
+    "truncated": false,
+    "forks_url": "https://api.github.com/gists/8f28fda67396a49a6c46b0964be4f5a8/forks",
+    "commits_url": "https://api.github.com/gists/8f28fda67396a49a6c46b0964be4f5a8/commits",
+    "user": null,
+    "public": true,
+    "comments_url": "https://api.github.com/gists/8f28fda67396a49a6c46b0964be4f5a8/comments"
+  }
+*/
 
 struct Gist: Decodable {
     var id: String?
     var url: String?
     var description: String?
     var createdAt: String?
-    var nodeId: Int?
+    var nodeId: String?
     var comments: Int?
     
     var isFavorite: Bool = false
