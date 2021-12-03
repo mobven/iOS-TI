a681754527ba0f6edc4b038ed85416ee58a018ed
Mustafa MEDENi <mustafa@medeni.dev>
2021-12-03 13:03:54 +0300

    feature/MBIOS-9.1

--- MobvenIOSTI/Config/VersionConfig.swift
+++ MobvenIOSTI/Config/VersionConfig.swift
@@ -41,6 +41,8 @@ final class VersionConfig {
     /// Brings already created version label to the front of the current window.
     func show() {
         UIApplication.shared.delegate?.window??.bringSubviewToFront(self.versionLabel)
+        versionLabel.layer.zPosition = CGFloat(Float.greatestFiniteMagnitude)
+
     }
     
 }
